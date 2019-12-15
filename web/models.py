708b5a595ae3c8515f46dad1fc4047a2dfa5d55b
mjhatami <mj.hatami626@gmail.com>
2019-12-15 10:10:24 +0330

    class name display better now on the admin page

--- web/models.py
+++ web/models.py
@@ -9,9 +9,14 @@ class Expense(models.Model):
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__ (self):
+        return "{}, amount: {}, time: {}".format(self.text, self.amount, self.date)
 
 class Income(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     User = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__ (self):
+        return "{}, amount: {}, time: {}".format(self.text, self.amount, self.date)
